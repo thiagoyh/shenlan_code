8c9aa01daee7c25c83d487067932da3ea41f3a2a
Thiagoyh <lwisyh@gmail.com>
2021-07-14 20:36:00 +0800

    delete trash

--- HW5/gaussian_newton_ws/src/gaussian_newton_scanmatcher/cmake-build-release/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "gaussian_newton_scanmatcher"
-PROJECT_SPACE_DIR = "/home/xcy/my_homework/HW5/gaussian_newton_ws/src/gaussian_newton_scanmatcher/cmake-build-release/devel"
-PROJECT_VERSION = "0.0.0"
--- HW5/gaussian_newton_ws/src/gaussian_newton_scanmatcher/cmake-build-release/gtest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/xcy/my_homework/HW5/gaussian_newton_ws/src/gaussian_newton_scanmatcher/cmake-build-release/gtest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
